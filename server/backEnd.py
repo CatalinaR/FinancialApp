070b0f8e1b739e8d9527a5a91caf13d981d32328
Catalina Rotaru <rotarucatalina991@gmail.com>
2021-07-11 22:04:23 +0100

    Inital app with basic functionality

---
+++ server/backEnd.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+
+def getDataForJson(stock):
+    dictDataStockTimeFrame = {}
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(stock, key)
+    response = requests.get(url)
+    responseJson = response.json()
+    seriesStock = responseJson["Time Series (Daily)"]
+    valuesCollect = []
+    for day in seriesStock:
+        values = seriesStock[day]
+        valuesCollect.append(values["1. open"])
+    return valuesCollect
+
+
+
+
---
+++ server/serverApi.py
@@ -0,0 +1,29 @@
+import flask
+import json
+from backEnd import getDataForJson
+
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Financial Data</h1>"
+
+@app.route('/data', methods=['GET'])
+def apple_data():
+    search = request.args.get("stock")
+    print("VAlues received: " + search)
+    if search is not None:
+        stock = search
+    else:
+        stock = 'AAPL'
+    dataCollect = getDataForJson(stock)
+    print(dataCollect)
+    data_set = {stock: dataCollect}
+    json_dumps = json.dumps(data_set)
+    return json_dumps
+
+app.run()
